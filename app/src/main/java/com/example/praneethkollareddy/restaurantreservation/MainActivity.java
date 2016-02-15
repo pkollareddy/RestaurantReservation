d2ca024087aab7007aba89f0832ab88093e17ae6
Anisha Kollareddy <bluestar225@gmail.com>
2016-02-14 21:33:26 -0800

    Created a log output in MainActivity to tests how pull requests can be created. At least 2 versions seem to be required.

--- app/src/main/java/com/example/praneethkollareddy/restaurantreservation/MainActivity.java
+++ app/src/main/java/com/example/praneethkollareddy/restaurantreservation/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                System.out.println("Im adding this to test GitHub changes to create a pull request.");
             }
         });
     }

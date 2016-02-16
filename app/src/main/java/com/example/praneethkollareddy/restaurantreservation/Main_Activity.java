d3378331ef1fb95a562ea900909895db8b451d8b
manroop <manroopmsingh@gmail.com>
2016-02-15 17:39:46 -0800

    Created new main activity and deleted the previous one. *test

--- app/src/main/java/com/example/praneethkollareddy/restaurantreservation/Main_Activity.java
+++ app/src/main/java/com/example/praneethkollareddy/restaurantreservation/Main_Activity.java
@@ -13,13 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Main2Activity extends AppCompatActivity
+public class Main_Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -55,7 +55,7 @@ public class Main2Activity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
+        getMenuInflater().inflate(R.menu.main_, menu);
         return true;
     }
 

88dcfb6d1e65d36de1c3ec4c57c47c13d7ca7615
manroop <manroopmsingh@gmail.com>
2016-02-20 15:41:54 -0800

    linked all activities

--- app/src/main/java/com/example/praneethkollareddy/restaurantreservation/ActResDetails.java
+++ app/src/main/java/com/example/praneethkollareddy/restaurantreservation/ActResDetails.java
@@ -1,13 +1,17 @@
 package com.example.praneethkollareddy.restaurantreservation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ActResDetails extends AppCompatActivity {
+
+    Button btn_reserve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,15 @@ public class ActResDetails extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        btn_reserve = (Button) findViewById(R.id.btn_reserve);
+        btn_reserve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(),MakeReservation.class);
+                startActivity(in);
+
+            }
+        });
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
